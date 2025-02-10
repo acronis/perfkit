5ff95d78bc353aec920e9d5b4de1ee7e28a9aba5
Ilya Kompaniets <ilya@kompaniets.dev>
2025-02-10 18:27:21 +0200

    Added skipping vector sql test for non-pg dialects

--- db/sql/vector_test.go
+++ db/sql/vector_test.go
@@ -54,6 +54,17 @@ func testVectorTableDefinition(dia db.DialectName) *db.TableDefinition {
 }
 
 func (suite *TestingSuite) TestVectorSearch() {
+	var actualDialect, err = dbDialect(suite.ConnString)
+	if err != nil {
+		suite.T().Error(err)
+		return
+	}
+
+	if actualDialect.name() != db.POSTGRES {
+		suite.T().Skip("only postgresql supports vector search")
+		return
+	}
+
 	d, s, c := suite.makeVectorTestSession()
 	defer logDbTime(suite.T(), c)
 	defer vectorCleanup(suite.T(), d)
